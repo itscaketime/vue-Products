4e06f2163a2c60aca52663d93b782d653df6896e
itscaketime <homers.band@gmail.com>
2022-07-31 19:02:21 +0300

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Layout>
       <template #top><h1 class="title">Добавление товара</h1></template>
       <template #bottom>
+        <ProductForm />
         <!-- form -->
         <!-- products -->
       </template>
@@ -12,25 +13,33 @@
 
 <script>
 import Layout from './components/Layout.vue';
-import { ref,computed } from 'vue';
-const IMGAE_URL = "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=640&q=80"
+import ProductForm from './components/ProductForm.vue';
+import { ref, computed } from 'vue';
+const IMGAE_URL =
+  'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=640&q=80';
 const genericItem = () => ({
-  id: "item:" + Math.floor(Math.random() * 100),
-  title: "Наименование товара " +  Math.floor(Math.random() * 100),
-  desc:
-    "Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк",
-  image:IMGAE_URL
-    ,
-  value: "10 000",
+  id: 'item:' + Math.floor(Math.random() * 100),
+  title: 'Наименование товара ' + Math.floor(Math.random() * 100),
+  desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+  image: IMGAE_URL,
+  value: '10 000',
 });
 export default {
   name: 'App',
   components: {
     Layout,
+    ProductForm,
   },
   setup() {
     const products = ref([]);
-    const filteredProducts = computed(()=> prdocuts)
+    // const filteredProducts = computed(()=> prdocuts);
+
+    const addProduct = (data) => {
+      products.value.push(data);
+    };
+    return {
+      products,
+    };
   },
 };
 </script>
@@ -43,5 +52,6 @@ export default {
 }
 .title {
   font-size: 1.75rem;
+  white-space: nowrap;
 }
 </style>
--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -1,5 +1,55 @@
 <template>
-  <form method="POST" id="product-form"></form>
+  <form method="POST" id="product-form" @submit.prevent="onFormSubmit">
+    <div class="form-field">
+      <label for="input-text"
+        ><span class="required">Наименование товара</span></label
+      >
+      <input
+        type="text"
+        id="input-text"
+        name="text"
+        placeholder="Введите наименование товара"
+        v-model="productName"
+      />
+    </div>
+    <div class="form-field">
+      <label for="input-desc">Описание товара</label>
+      <textarea
+        maxlength="100"
+        type="desc"
+        id="input-desc"
+        name="desc"
+        placeholder="Введите описание товара"
+        v-model="productDesc"
+      >
+      </textarea>
+    </div>
+    <div class="form-field">
+      <label for="input-value"
+        ><span class="required"
+          >Введите ссылку на изображение товара</span
+        ></label
+      >
+      <input
+        type="text"
+        id="input-image"
+        name="image"
+        placeholder="Введите ссылку"
+        v-model="productImage"
+      />
+    </div>
+    <div class="form-field">
+      <label for="input-value"><span class="required">Цена товара</span></label>
+      <input
+        type="text"
+        id="input-value"
+        name="value"
+        placeholder="Введите цену"
+        v-model="productPrice"
+      />
+    </div>
+    <button class="add-btn" type="submit">Добавить товар</button>
+  </form>
 </template>
 
 <script>
@@ -7,11 +57,61 @@ import { ref } from 'vue';
 export default {
   name: 'ProductForm',
   components: {},
-  setup() {},
+  setup() {
+    const productName = ref('');
+    const productDesc = ref('');
+    const productImage = ref('');
+    const productPrice = ref('');
+
+    const onFormSubmit = (ev) => {
+      console.log('form submit');
+    };
+    return {
+      productName,
+      productDesc,
+      productImage,
+      productPrice,
+      onFormSubmit,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 #product-form {
+  background-color: white;
+  z-index: 1000;
+  width: 20rem;
+  display: block;
+  padding: 1.5rem;
+  border-radius: 0.5rem;
+  position: fixed;
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04),
+    0px 6px 10px rgba(0, 0, 0, 0.02);
+  .form-field {
+    margin-bottom: 1rem;
+    &:last-child {
+      margin-bottom: none;
+    }
+  }
+  .required {
+    position: relative;
+    &::after {
+      content: '';
+      width: 4px;
+      height: 4px;
+      display: block;
+      border-radius: 100%;
+      background-color: crimson;
+      position: absolute;
+      right: -4px;
+      top: -2px;
+    }
+  }
+  .add-btn {
+    width: 100%;
+  }
+  .err {
+  }
 }
 </style>
