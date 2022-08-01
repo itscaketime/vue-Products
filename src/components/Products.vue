20c966f0d7e47896e2431204448bd2b170d38481
itscaketime <homers.band@gmail.com>
2022-08-01 09:18:19 +0300

    2 files modified, 1 added

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,7 +4,7 @@
       v-for="product in products"
       :key="product.id"
       :product="product"
-      @remove-product="$emit('remove-product',product.id)"
+      @remove-product="$emit('remove-product', product.id)"
     />
   </div>
 </template>
@@ -15,7 +15,7 @@ import { ref } from 'vue';
 export default {
   name: 'Products',
   props: ['products'],
-  emits:['remove-product'],
+  emits: ['remove-product'],
   components: {
     ProductCard,
   },
@@ -29,5 +29,20 @@ export default {
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 1rem;
   margin-left: 21rem;
+  @media screen and (max-width: 1440px) {
+    & {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+  @media screen and (max-width: 1024px) {
+    & {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+  // @media screen and (max-width: 736px) {
+  //   & {
+  //     margin-left: 0;
+  //   }
+  // }
 }
 </style>
